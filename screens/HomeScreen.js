6d564e62d5158436a989df19673830dc13546462
Len Ganley <lenjganley@gmail.com>
2023-01-14 00:43:22 -0500

    Added component to render nav options. OnPress event needs attention.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,38 +1,66 @@
-import { Button, ScrollView } from "react-native"
+import { View, StyleSheet } from "react-native"
+import { FlatList } from "react-native-gesture-handler"
+import { ListItem, Card } from 'react-native-elements' 
 
-const Favorites = () => {
+const homeNav = [
+    {
+        id: 0,
+        name: 'Help Me Choose',
+        component: 'HelpMeChoose'
+    },
+    {
+        id: 1,
+        name: 'Favorites!',
+        component: 'Favorites'
+    },
+    {
+        id: 2,
+        name: 'What\'s New?',
+        component: 'WhatsNew'
+    },
+    {
+        id: 3,
+        name: 'Browse Library',
+        component: 'BrowseLibrary'
+    }
+]
+
+const NavCatalogue = (props) => {
+    const renderButton = ({ item }) => {
+        return (
+            <ListItem onPress={()=>props.onPress(item.id)}>
+                <Card style={styles.navCard}>
+                    <Card.Title>{item.name}</Card.Title>
+                </Card>
+            </ListItem>
+        )
+    }
     return (
-        <Button title="Favorites!" />
+        <Card title="Nav Buttons">
+            <FlatList
+                data={homeNav}
+                renderItem={renderButton}
+                keyExtractor={item=>item.id.toString()}
+            />
+        </Card>
     )
 }
 
-const BrowseLibrary = () => {
-    return (
-        <Button title="Browse Library" />
-    )
-}
-
-const HelpMeChoose = () => {
-    return (
-        <Button title="Help Me Choose" />
-    )
-}
-
-const WhatsNew = () => {
-    return (
-        <Button title="What's New?" />
-    )
-}
 
 const HomeScreen = () => {
     return(
-        <ScrollView>
-            <HelpMeChoose />
-            <Favorites />
-            <WhatsNew />
-            <BrowseLibrary />
-        </ScrollView>
+        <View style={{alignItems:'center'}}>
+            <NavCatalogue />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    navCard: {
+        flex: 1,
+        padding: 3,
+        color: '#1A748E'
+    },
+})
 
 export default HomeScreen
